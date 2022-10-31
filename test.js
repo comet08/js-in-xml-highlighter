1e71d3c385d354f5edeb7aef2c543e2fa1b6934f
comet <sperocometa@naver.com>
2022-10-31 23:00:03 +0900

    [Init]

---
+++ test.js
@@ -0,0 +1,36 @@
+if(test) {
+    return true;
+}
+
+0235
+
+onclick
+
+
+test abcd
+
+onload
+
+test => {
+
+}
+
+function()
+
+
+break; else
+
+let
+
+const
+
+
+for(var i = 0 ; i < test; i++) {
+var let const
+}
+continue;
+
+
+test,t 
+
+switch()}{}
